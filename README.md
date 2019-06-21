8c43a8f1908e8fb5cb75dc514bca55a6fa0c71ca
Ibrahim Saberi <georgeipsum@gmail.com>
2019-06-21 16:06:01 -0700

    thanks! I hate it

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # dl-vimeo-account
-npm utility that downloads all source files from a vimeo account with a provided vimeo API access token
+## A node script that downloads all video files (highest transcoded quality) from your vimeo account
+
+Targets Vimeo API version 3.2
+
+### Who would ever need this?
+
+Anyone with a Vimeo business account with >20 videos who needs to download all of those videos for whatever reason. That reason definitely would never have anything to do with migrating away from Vimeo. Nobody would *ever* do that.
+
+### What you'll need:
+
+* A Vimeo business/ pro account (to access file download links from the developer api)
+* A Vimeo API App set up on the [Vimeo developer portal](https://developer.vimeo.com/apps)
+* That Vimeo API App's [Client ID](#client-id)
+* Vimeo API App [Client Secret](#client-secret)
+* Vimeo API App [Personal Access Token with the `video_files` scope](#personal-access-token)
+
+__NOTE: If you don't have a Vimeo business account I don't believe you can use this. Vimeo only gives download link accessibility via the Vimeo API to Business/PRO accounts as far as I know.__ (please let me know if this isn't true, or if there's a way around this limitation)
+
+#### How to use:
+
+* Clone this, download this, or (__if you're a heathen__) open up your favorite text editor and rewrite/ copy each file over to the same directory
+* Do _not_ look too closely at `index.js`. It's Really Not Good Programming and half the time I spent making this went into making that gif in the readme look _real nice_.
+  * Also I'm baby
+    * Feel free to tell me I'm terrible but please tell me why
+* Run `npm install`
+  * If you don't know what that means [check this out](https://www.npmjs.com/get-npm)
+* Run `node index.js` or `npm start` (what? you want to install this on your path? let me know and I'll make this less terrible to use)
+* You'll be asked for your app's access token, client id, and client secret in the command line. Input them. If you don't specify this the utility can't fetch the file data from the Vimeo API.
+* You'll be asked for a filepath to save the videos and log output. If you don't specify an absolute file path (i.e. with a leading `/`) the path will resolve relative to the directory that `index.js` is located in. I think. I haven't actually tested this out.
+  * If you don't specify a filepath, your videos will be saved in the directory where `index.js` is located under the subdirectory `videos`.
+
+If you use the Vimeo API to upload, you might have some videos that are corrupted (and you haven't deleted them yet because you have better things to do than navigate to vimeo.com). Corruption can happen either during the transcoding process or in the upload process or perhaps when __someone tries to upload a fucking PDF as a video__.
+
+![web development was a mistake.jpeg_large](./assets/pdf.jpg)
+
+In those cases Vimeo will still recognize that the video exists but there will be no video files or they'll come in as some unrecognizable and unusable format. For any video where this happens to be the case it'll log that to the command line and then also to a `LOG` file which is placed wherever you specified where your videos should be downloaded to.
+
+### What EXACTLY is being downloaded?
+
+Each Vimeo video has a uri; that uri serves as the filename plus whatever extension is in use for that video. The script will attempt to take the highest quality (aka largest width) Vimeo transcoded video. That means it will not download the actual source file that you originally uploaded (this is a totally separate thing!). 
+
+Also, it's unclear whether QHD (aka 2K or 1440P) or UHD (4K or 2160P) videos are detected properly but I'm guessing that the script will download the 1080P versions of those. Will fix in the future.
+
+### What? Why wouldn't you download the source video when possible?
+
+Because I made this for _me_ and I already have all of my source videos.
+
+In reality I didn't want to deal with the edge case of source files being corrupted or missing, and all of my source files were at most 1080P, so the Vimeo transcoded versions always worked for me.
+
+### Huh? Why would you need this if you already have all of your source videos?
+
+I needed the transcoded versions that are on Vimeo as well because of reasons.
+
+### This script is terrible. It {crashed, didn't work, stopped working in the middle of the download process}!
+
+I know! It sucks! Especially that third situation if you have, say, __Literally Five Thousand Videos To Download__.
+
+![Literally Over Four Thousand Nine Hundred Ninety Nine Videos.png_yuge](./assets/its-over-4999.png)
+
+In the former 2 cases, create an issue with whatever output node spits out and I'll look into it. In the latter case, still create an issue to let me know what happened, but the script will also save all of your file data and keep note of how far you are in the process, so if you rerun the script you _should_ in theory be able to just resume the download process. Let me know if that isn't the case.
+
+## What You'll Need (but with pictures!)
+
+### Client ID
+
+`Home > My Apps > {APP NAME} > (In Sidebar) General Information`
+
+It's the first thing you see (marked __Client identifier__):
+
+![client id image](./assets/client-id.png)
+
+### Client Secret
+
+`Home > My Apps > {APP NAME} > (In Sidebar) Authentication > Manage App Secrets`
+
+Marked __Client secrets__ (if you have multiple you should be able to use any of them):
+
+![client secret image](./assets/client-secret.png)
+
+### Personal Access Token
+
+`Home > My Apps > {APP NAME} > (In Sidebar) Authentication > Personal Access Tokens`
+
+If you don't currently have a personal access token with the `video_files` scope, you'll need to create one:
+
+![personal access token gif](./assets/personal-access-token.gif)
+
+It should go without saying but I've deleted that token. __Don't try to use it el oh el__
--- index.js
+++ index.js
@@ -10,7 +10,64 @@ const Spinner = require("cli-spinner").Spinner
 const axios = require("axios")
 const Vimeo = require("vimeo").Vimeo
 
-const spinner = new Spinner("%s Loading Videos from API... ")
+const spinner = new Spinner("%s Checking for previous download info... ")
+spinner.setSpinnerDelay(100)
+var LOGPATH
+var ID
+var SECRET
+var TOKEN
+var FILEPATH
+
+var TOTALSIZE = 0
+var TOTALBYTESDOWNLOADED = 0
+var TOTALFILESDOWNLOADED = 0
+var TOTALFILES
+var TOTALPAGES
+var FILES = []
+var TOTAL_LOADING_DONE = 0
+
+//what the fuck are global variables. what do you mean this is a bad
+
+const run = async () => {
+  init()
+
+  spinner.start()
+
+  try {
+    spinner.stop()
+    if(fs.existsSync('files.json')) {
+      console.log("")
+
+      const answers = await askContinue()
+
+      if(answers.RESTART.toLowerCase()=="y"||answers.RESTART.toLowerCase()=="yes") {
+        const files = require(path.resolve(__dirname,"files.json"))
+        ID = files.ID
+        SECRET = files.SECRET
+        TOKEN = files.TOKEN
+        FILEPATH = files.FILEPATH
+        TOTALFILES = files.TOTALFILES
+        TOTALFILESDOWNLOADED = files.TOTALFILESDOWNLOADED
+        TOTALBYTESDOWNLOADED = files.TOTALBYTESDOWNLOADED
+        FILES = files.FILES
+
+        fs.writeFile(path.resolve(__dirname,FILEPATH,'LOG'),"",(err) => {
+          if(err) throw err
+          LOGPATH = path.resolve(__dirname,FILEPATH,'LOG')
+          downloadFiles()
+        })
+      } else {
+        console.log("")
+        runTraditional()
+      }
+    } else {
+      console.log("")
+      runTraditional() 
+    }
+  } catch(err) {
+    console.error(err)
+  }
+}
 
 const init = () => {
   console.log(
@@ -24,6 +81,11 @@ const init = () => {
   )
   console.log(" ")
   console.log(" ")
+}
+
+const exitCantCreateClient = () => {
+  console.log("Something went wrong... we can't create the vimeo client!")
+  process.exit()
 }
 
 const askQuestions = () => {
@@ -42,94 +104,237 @@ const askQuestions = () => {
       name: "SECRET",
       type: "input",
       message: "Input client secret:"
+    },
+    {
+      name: "FILEPATH",
+      type: "input",
+      message: "Specify filepath to save videos to:"
     }
   ]
   
   return inquirer.prompt(questions)
 }
 
+const askContinue = () => {
+  const filesJson = require(path.resolve(__dirname,"files.json"))
+  const questions = [
+    {
+      name: "RESTART",
+      type: "input",
+      message: `Would you like to continue your download? ${filesJson.TOTALFILESDOWNLOADED}/${filesJson.TOTALFILES} videos downloaded (y/n)`
+    }
+  ]
+
+  return inquirer.prompt(questions)
+}
+
+const downloadFiles = () => {
+  spinner.setSpinnerTitle("%s Preparing files for download...")
+  FILES.forEach(e => {
+    spinner.isSpinning() ? null : spinner.start()
+    let uri = (e.uri.split("/"))[2]
+    if(e.files.length) {
+      let dlLink = false
+      let fileList = e.files.filter(e => e.quality=='hd'||e.quality=='sd')
+      let fileToUse
+      if(fileList.length) {
+        fileList.sort((a,b) => b.width-a.width)
+        fileToUse = fileList[0]
+        dlLink = fileToUse.link
+      }
+  
+      if(dlLink) {
+        let type = (fileToUse.type.split("/"))[1]
+        let filename = uri.concat(".",type)
+        e.trueURI = uri
+        e.filename = filename
+        e.link = dlLink
+        TOTALSIZE += fileToUse.size
+      } else {
+        fs.open(LOGPATH,'a',777,(e,id) => {
+          fs.write(id, `Error getting download link for uri ${uri}\n`,null,'utf8',() => {
+            fs.close(id, () => {
+              console.log("")
+              console.log(`Error getting download link for uri ${uri}`)
+            })
+          })
+        })
+      }
+    } else {
+      fs.open(LOGPATH,'a',777,(e,id) => {
+        fs.write(id, `Files corrupted or missing for uri ${uri}\n`,null,'utf8',() => {
+          fs.close(id, () => {
+            console.log("")
+            console.log(`Files corrupted or missing for uri ${uri}`)
+          })
+        })
+      })
+    }
+  })
+
+  console.log("")
+  spinner.setSpinnerTitle("%s Downloading files...")
+  dl(FILES[TOTALFILESDOWNLOADED].link,FILEPATH,FILES[TOTALFILESDOWNLOADED].filename)
+}
+
+const dl = async (link,filepath,filename) => {
+  if(link) {
+    let filesJson = require(path.resolve(__dirname,"files.json"))
+    const writer = fs.createWriteStream(path.resolve(__dirname,filepath,filename))
+    const response = await axios(
+      {
+        url:link,
+        method:'GET',
+        responseType:'stream'
+      }
+    )
+
+    spinner.isSpinning() ? null : spinner.start()
+    response.data.on('data', (data) => {
+      TOTALBYTESDOWNLOADED += Buffer.byteLength(data)
+      const progress = 3*Math.round(100*(TOTALBYTESDOWNLOADED/TOTALSIZE))/10
+      spinner.setSpinnerTitle(`%s Downloaded ${TOTALFILESDOWNLOADED}/${TOTALFILES} FILES | ${Math.round(10*TOTALBYTESDOWNLOADED/(1024*1024*1024))/10}GB/${Math.round(10*TOTALSIZE/(1024*1024*1024))/10}GB |${"=".repeat(Math.round(progress))}${"-".repeat(Math.round(30-progress))}|`)
+    })
+    response.data.on('end', ()=> {
+      TOTALFILESDOWNLOADED++
+      filesJson.TOTALFILESDOWNLOADED = TOTALFILESDOWNLOADED
+      filesJson.TOTALBYTESDOWNLOADED = TOTALBYTESDOWNLOADED
+      if(TOTALFILESDOWNLOADED==TOTALFILES) {
+        filesJson = JSON.stringify(filesJson, null, 2)
+        fs.writeFile('files.json',filesJson, (err) => {
+          if(err) throw err
+          finish()
+        })
+      } else {
+        filesJson = JSON.stringify(filesJson, null, 2)
+        fs.writeFile('files.json',filesJson, (err) => {
+          if(err) throw err
+          dl(FILES[TOTALFILESDOWNLOADED].link,FILEPATH,FILES[TOTALFILESDOWNLOADED].filename)
+        })
+      }
+    })
+    response.data.pipe(writer)
+  
+    return new Promise((resolve,reject) => {
+      writer.on('finish', resolve)
+      writer.on('error', reject)
+    })
+  } else {
+    fs.open(LOGPATH,'a',777,(e,id) => {
+      fs.write(id, `Did not download file ${FILES[TOTALFILESDOWNLOADED].uri}\n`,null,'utf8',() => {
+        fs.close(id, () => {
+          console.log("")
+          console.log(`Did not download file ${FILES[TOTALFILESDOWNLOADED].uri}`)
+        })
+      })
+    })
+    TOTALFILESDOWNLOADED++
+    filesJson.TOTALFILESDOWNLOADED = TOTALFILESDOWNLOADED
+    filesJson = JSON.stringify(filesJson, null, 2)
+    fs.writeFile('files.json',filesJson, (err) => {if(err) throw err})
+    dl(FILES[TOTALFILESDOWNLOADED].link,FILEPATH,FILES[TOTALFILESDOWNLOADED].filename)
+  }
+}
+
+const finish = () => {
+  spinner.stop()
+  if(TOTALFILES > 200) {
+    console.log("Oh thank goodness. It's over. It's finally over.")
+  } else {
+    console.log("Done! Thanks for using dl-vimeo!")
+  }
+  process.exit()
+}
+
+const recordFiles = (files) => {
+  let filesToRecord = {
+    ID: ID,
+    SECRET: SECRET,
+    FILEPATH: FILEPATH,
+    TOTALFILES: TOTALFILES,
+    TOTALFILESDOWNLOADED: TOTALFILESDOWNLOADED,
+    TOTALBYTESDOWNLOADED: TOTALBYTESDOWNLOADED,
+    FILES: files
+  }
+
+  filesToRecord = JSON.stringify(filesToRecord, null, 2)
+
+  fs.writeFile('files.json', filesToRecord, (err) => { 
+    if(err) throw err
+    console.log("")
+    console.log("File data written to files.json")
+    downloadFiles()
+  })
+}
+
+const loadVideoData = (pages) => {
+  const vimeoClient = ID ? new Vimeo(ID, SECRET, TOKEN) : exitCantCreateClient()
+  for(let i=1; i<=pages; i++) {
+    vimeoClient.request({
+      method:'GET',
+      path:`/me/videos?per_page=100&page=${i}`,
+      query: {
+        per_page:100,
+        fields:"uri,files"
+      }
+    },(error,body,status_code,headers) => {
+      if(error) {
+        console.log(error)
+        console.log(`HTTP STATUS CODE: ${status_code}`)
+        process.exit()
+      } else {
+        body.data.forEach(e => {
+          FILES.push(e)
+          TOTAL_LOADING_DONE++
+          spinner.setSpinnerTitle(`%s Loading all video data from Vimeo API... ${TOTAL_LOADING_DONE}/${TOTALFILES}`)
+        })
+        if(TOTAL_LOADING_DONE==TOTALFILES) {
+          spinner.setSpinnerTitle("%s Loading complete! Making sure everything is in order... ")
+          recordFiles(FILES)
+        }
+      }
+    })
+  }
+}
+
 const vimeoInitialTest = (error, body, status_code, headers) => {
   spinner.isSpinning() ? spinner.stop() : null
   if(error) {
     console.log(error)
-    return error
+    process.exit()
   } else {
     console.log(`total videos found: ${body.total}`)
-    for(var i=0; i<body.data[0].files.length; i++) {
-      let e = body.data[0].files[i]
-      if(e.quality==='hd' && e.width===1920) {
-        const response = axios(
-          {
-            url:"https://player.vimeo.com/external/340289751.hd.mp4?s=99620937b375a93a1a021a389f804842f638a6db&profile_id=175&oauth2_token_id=1225356879",
-            method:'GET',
-            responseType:'stream'
-          }
-        )
-
-        response.data.pipe(fs.createWriteStream(path.resolve(__dirname)))
-      } else {
-
-      }
-    }
-    return body
+    TOTALFILES = body.total
+    TOTALPAGES = Math.ceil(body.total/100)
+    spinner.setSpinnerTitle("%s Loading all video data from Vimeo API... ")
+    spinner.start()
+    loadVideoData(TOTALPAGES)
   }
 }
 
-const success = () => {
-  return 0
-}
-
-const run = async () => {
-  init()
-
+const runTraditional = async () => {
   const answers = await askQuestions()
-  let { ID, SECRET, TOKEN } = answers
+  ID = answers.ID
+  SECRET = answers.SECRET
+  TOKEN = answers.TOKEN
+  FILEPATH = answers.FILEPATH
+  FILEPATH = FILEPATH=="" ? "videos/" : FILEPATH
+
+  fs.writeFile(path.resolve(__dirname,FILEPATH,'LOG'),"",(err) => {
+    if(err) throw err
+    LOGPATH = path.resolve(__dirname,FILEPATH,'LOG')
+  })
 
   const vimeoClient = new Vimeo(ID, SECRET, TOKEN)
 
-  asdf = vimeoClient.request({
+  vimeoClient.request({
     method: 'GET',
     path: '/me/videos',
     query: {
       per_page: 1,
-      fields: "files"
+      fields: "uri"
     }
-  }, vimeoInitialTest)
-  
-  spinner.start()
+  }, vimeoInitialTest)      
 }
 
-//run()
-
-async function dl() {
-  const writer = fs.createWriteStream(path.resolve(__dirname,'asdf.mp4'))
-  const response = await axios(
-    {
-      url:"https://player.vimeo.com/external/340289751.hd.mp4?s=99620937b375a93a1a021a389f804842f638a6db&profile_id=175&oauth2_token_id=1225356879",
-      method:'GET',
-      responseType:'stream'
-    }
-  )
-
-  const size = response.headers['content-length']
-  console.log(size)
-  let downloaded = 0
-  spinner.start()
-  response.data.on('data', (data) => {
-    downloaded += Buffer.byteLength(data)
-    spinner.setSpinnerTitle(`progress: ${Math.round(downloaded/(1024*1024))}/${Math.round(size/(1024*1024))}`)
-  })
-  response.data.on('end', ()=> {
-    spinner.stop()
-    console.log(" ")
-    console.log("IT'S DONE")
-  })
-  response.data.pipe(writer)
-
-  return new Promise((resolve,reject) => {
-    writer.on('finish', resolve)
-    writer.on('error', reject)
-  })
-}
-
-dl()
+run()
